bf1d47eda91219145c0b328ad56df27fd04f0479
banerjeeanirban <banerjee.anirban@gmail.com>
2024-09-04 08:35:36 -0500

    Update httpclient.cs
    changed riscology - to - oracle

--- httpclient.cs
+++ httpclient.cs
@@ -10,7 +10,7 @@ var content = new FormUrlEncodedContent(values);
 # Post Request
 var response = await client.PostAsync("http://microsoft.com/rino/?horns=yes", content);
 
-var response = client.PostAsync("https://api.riscology.com/urlpath", new StringContent(JsonConvert.SerializeObject(data, hello), Encoding.UTF8, "application/json")).Result;
+var response = client.PostAsync("https://api.oracle.com/urlpath", new StringContent(JsonConvert.SerializeObject(data, hello), Encoding.UTF8, "application/json")).Result;
 var responseString = await client.GetStringAsync(TestURL);
 
 var response = client.PostAsync("/urlpath", new StringContent(JsonConvert.SerializeObject(data, worlds), Encoding.UTF8, "application/json")).Result;
